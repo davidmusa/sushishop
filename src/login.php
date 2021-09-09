444ea89d6ba27cd24346dd7f46b0e2b9e8a4d026
David M <davidmusa1@hotmail.com>
2021-09-09 14:43:08 -0400

    Modified lines

--- src/login.php
+++ src/login.php
@@ -1,5 +1,43 @@
 <?php
 
-echo "cool";
+session_start();
+
+    if (isset($_POST['amount1'])){
+        echo $_POST['amount1']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount2'])){
+        echo $_POST['amount2']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount3'])){
+        echo $_POST['amount3']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount4'])){
+        echo $_POST['amount4']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount5'])){
+        echo $_POST['amount5']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount6'])){
+        echo $_POST['amount6']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount7'])){
+        echo $_POST['amount7']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount8'])){
+        echo $_POST['amount8']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount9'])){
+        echo $_POST['amount9']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount10'])){
+        echo $_POST['amount10']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount11'])){
+        echo $_POST['amount11']; // Displays value of checked checkbox.
+        }
+    if (isset($_POST['amount12'])){
+        echo $_POST['amount12']; // Displays value of checked checkbox.
+        }
+                                            
 
 ?>
