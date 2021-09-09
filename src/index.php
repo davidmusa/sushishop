21018159ba25f04c1513f6932af588df9dea5de2
David M <davidmusa1@hotmail.com>
2021-09-09 14:24:36 -0400

    Added Values

--- src/index.php
+++ src/index.php
@@ -14,41 +14,41 @@
 
   <p>Nigiri- Please choose amount</p>
   <input type="checkbox" id="six1" name="amount1" value="6">
-  <label for="6">6</label><br>
+  <label for="6">6 - 6$</label><br>
   <input type="checkbox" id="twelve1" name="amount2" value="12">
-  <label for="12">12</label><br>
+  <label for="12">12 - 11$</label><br>
   <input type="checkbox" id="twenty1" name="amount3" value="20">
-  <label for="20">20</label><br>
+  <label for="20">20 - 18$</label><br>
 
   <br>  
 
   <p>California- Please choose amount</p>
   <input type="checkbox" id="six2" name="amount4" value="6">
-  <label for="6">6</label><br>
+  <label for="6">6 - 6$</label><br>
   <input type="checkbox" id="twelve2" name="amount5" value="12">
-  <label for="12">12</label><br>
+  <label for="12">12 - 11$</label><br>
   <input type="checkbox" id="twenty2" name="amount6" value="20">
-  <label for="20">20</label><br>
+  <label for="20">20 - 18$</label><br>
 
   <br>  
 
   <p>Avocado- Please choose amount</p>
   <input type="checkbox" id="six3" name="amount7" value="6">
-  <label for="6">6</label><br>
+  <label for="6">6 - 6$</label><br>
   <input type="checkbox" id="twelve3" name="amount8" value="12">
-  <label for="12">12</label><br>
+  <label for="12">12 - 11$</label><br>
   <input type="checkbox" id="twenty3" name="amount9" value="20">
-  <label for="20">20</label><br>
+  <label for="20">20 - 18$</label><br>
 
   <br>  
 
   <p>Tempura- Please choose amount</p>
   <input type="checkbox" id="six4" name="amount10" value="6">
-  <label for="6">6</label><br>
+  <label for="6">6 - 6$</label><br>
   <input type="checkbox" id="twelve4" name="amount11" value="12">
-  <label for="12">12</label><br>
+  <label for="12">12 - 11$</label><br>
   <input type="checkbox" id="twenty4" name="amount12" value="20">
-  <label for="20">20</label><br>
+  <label for="20">20 - 18$</label><br>
 
   <br><br>
 
