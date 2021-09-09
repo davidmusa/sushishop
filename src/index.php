d97ebf5ce145161c6ca5099cc8977a1ef444d9a2
David M <davidmusa1@hotmail.com>
2021-09-09 15:53:28 -0400

    Made a lot of changes

--- src/index.php
+++ src/index.php
@@ -8,59 +8,25 @@
 </head>
 <body>
     MENU for SUSHISHOP
-        <form action="login.php" method="post">
-            
-            <div>
+    <form action="login.php" method="post">
+            <p>Nigiri- Please choose amount</p>
+            <input type="number" id="six1" name="nigiri" value="0">
+            <br>
 
-  <p>Nigiri- Please choose amount</p>
-  <input type="checkbox" id="six1" name="amount1" value="6 Nigiri">
-  <label for="6">6 - 6$</label><br>
-  <input type="checkbox" id="twelve1" name="amount2" value="12 Nigiri">
-  <label for="12">12 - 11$</label><br>
-  <input type="checkbox" id="twenty1" name="amount3" value="20 Nigiri">
-  <label for="20">20 - 18$</label><br>
+            <p>California- Please choose amount</p>
+            <input type="number" id="six1" name="california" value="0">
+            <br>
 
-  <br>  
+            <p>Tempura- Please choose amount</p>
+            <input type="number" id="six1" name="tempura" value="0">
+            <br>
 
-  <p>California- Please choose amount</p>
-  <input type="checkbox" id="six2" name="amount4" value="6 California">
-  <label for="6">6 - 6$</label><br>
-  <input type="checkbox" id="twelve2" name="amount5" value="12 California">
-  <label for="12">12 - 11$</label><br>
-  <input type="checkbox" id="twenty2" name="amount6" value="20 California">
-  <label for="20">20 - 18$</label><br>
+            <p>Avocado- Please choose amount</p>
+            <input type="number" id="six1" name="avocado" value="0">
+            <br>
 
-  <br>  
-
-  <p>Avocado- Please choose amount</p>
-  <input type="checkbox" id="six3" name="amount7" value="6 Avocado">
-  <label for="6">6 - 6$</label><br>
-  <input type="checkbox" id="twelve3" name="amount8" value="12 Avocado">
-  <label for="12">12 - 11$</label><br>
-  <input type="checkbox" id="twenty3" name="amount9" value="20 Avocado">
-  <label for="20">20 - 18$</label><br>
-
-  <br>  
-
-  <p>Tempura- Please choose amount</p>
-  <input type="checkbox" id="six4" name="amount10" value="6 Tempura">
-  <label for="6">6 - 6$</label><br>
-  <input type="checkbox" id="twelve4" name="amount11" value="12 Tempura">
-  <label for="12">12 - 11$</label><br>
-  <input type="checkbox" id="twenty4" name="amount12" value="20 Tempura">
-  <label for="20">20 - 18$</label><br>
-
-  <br><br>
-
-  <input type="submit" value="Submit">
-</form>
+            <input type="submit" value="Submit">
+        </form>
 
 </body>
-</html>
-
-
-            </div>
-
-        </form>
-    </body>
 </html>
--- src/login.php
+++ src/login.php
@@ -2,42 +2,100 @@
 
 session_start();
 
-    if (isset($_POST['amount1'])){
-        echo $_POST['amount1']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount2'])){
-        echo $_POST['amount2']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount3'])){
-        echo $_POST['amount3']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount4'])){
-        echo $_POST['amount4']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount5'])){
-        echo $_POST['amount5']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount6'])){
-        echo $_POST['amount6']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount7'])){
-        echo $_POST['amount7']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount8'])){
-        echo $_POST['amount8']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount9'])){
-        echo $_POST['amount9']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount10'])){
-        echo $_POST['amount10']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount11'])){
-        echo $_POST['amount11']; // Displays value of checked checkbox.
-        }
-    if (isset($_POST['amount12'])){
-        echo $_POST['amount12']; // Displays value of checked checkbox.
-        }
-                                            
+//1. INPUT
+//2. VALIDATE INPUT
+//3. BUSINESS LOGIC
+//4. OUTPUT
 
+$validated = false;
+
+    $n = 0;
+    $c = 0;
+    $t = 0;
+    $a = 0;
+
+    $error = "";
+
+if(isset($_POST['submit'])){
+    $temp_nigiri = htmlspecialchars($_POST['nigiri']);
+    $temp_california = htmlspecialchars($_POST['california']);
+    $temp_tempura = htmlspecialchars($_POST['tempura']);
+    $temp_avocado = htmlspecialchars($_POST['avocado']);
+
+    if(is_numeric($temp_nigiri) == true){
+        if ($temp_nigiri >= 0){
+            if($temp_nigiri <= 50){
+            $n = $temp_nigiri;
+        }else{
+            $error .= "<br>Maximum number is reached on X";
+            }
+        }else{
+            $error .= "<br>X is lower than 0";
+            }
+        }else{
+            $error .= "<br>Please input a number on X";
+            }                
+    
+    if(is_numeric($temp_california) == true){
+        if ($temp_california >= 0){
+            if($temp_california <= 50){
+            $c = $temp_california;
+        }else{
+            $error .= "<br>Maximum number is reached on Y";
+            }
+        }else{
+            $error .= "<br>Y is lower than 0";
+            }
+        }else{
+            $error .= "<br>Please input a number on Y";
+            }
+            
+    if(is_numeric($temp_tempura) == true){
+        if ($temp_tempura >= 0){
+            if($temp_tempura <= 50){
+            $t = $temp_tempura;
+        }else{
+            $error .= "<br>Maximum number is reached on Y";
+            }
+        }else{
+            $error .= "<br>Y is lower than 0";
+            }
+        }else{
+            $error .= "<br>Please input a number on Y";
+            }
+    
+    if(is_numeric($temp_avocado) == true){
+        if ($temp_avocado >= 0){
+            if($temp_avocado <= 50){
+            $a = $temp_avocado;
+        }else{
+            $error .= "<br>Maximum number is reached on Y";
+            }
+        }else{
+            $error .= "<br>Y is lower than 0";
+            }
+        }else{
+            $error .= "<br>Please input a number on Y";
+            }
+
+    if(empty($error)){
+                $validated = true;
+            } 
+        }
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receipt</title>
+</head>
+<body>
+    <div style="font-size: 50px;">Receipt</div>
+// 1 echo only
+</body>
+</html>
+    
