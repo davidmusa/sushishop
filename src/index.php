a89dab31f2c45f9efafb658376e442a400021e51
David M <davidmusa1@hotmail.com>
2021-09-09 13:50:10 -0400

    Modified index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,66 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SushiShop</title>
+</head>
+<body>
+    MENU for SUSHISHOP
+        <form action="login.php" method="post">
+            
+            <div>
 
-echo "hello hell";
-echo "123";
+  <p>Nigiri- Please choose amount</p>
+  <input type="radio" id="six" name="amount1" value="6">
+  <label for="6">6</label><br>
+  <input type="radio" id="twelve" name="amount2" value="12">
+  <label for="12">12</label><br>
+  <input type="radio" id="twenty" name="amount3" value="20">
+  <label for="20">20</label><br>
 
-?>
+  <br>  
+
+  <p>California- Please choose amount</p>
+  <input type="radio" id="six" name="amount1" value="6">
+  <label for="6">6</label><br>
+  <input type="radio" id="twelve" name="amount2" value="12">
+  <label for="12">12</label><br>
+  <input type="radio" id="twenty" name="amount3" value="20">
+  <label for="20">20</label><br>
+
+  <br>  
+
+  <p>Avocado- Please choose amount</p>
+  <input type="radio" id="six" name="amount1" value="6">
+  <label for="6">6</label><br>
+  <input type="radio" id="twelve" name="amount2" value="12">
+  <label for="12">12</label><br>
+  <input type="radio" id="twenty" name="amount3" value="20">
+  <label for="20">20</label><br>
+
+  <br>  
+
+  <p>Tempura- Please choose amount</p>
+  <input type="radio" id="six" name="amount1" value="6">
+  <label for="6">6</label><br>
+  <input type="radio" id="twelve" name="amount2" value="12">
+  <label for="12">12</label><br>
+  <input type="radio" id="twenty" name="amount3" value="20">
+  <label for="20">20</label><br>
+
+  <br><br>
+
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
+
+
+            </div>
+
+        </form>
+    </body>
+</html>
