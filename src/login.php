655eb56e5b1a243ed706ff79ffa583c57fec3e52
David M <davidmusa1@hotmail.com>
2021-09-10 11:27:57 -0400

    Adding more

--- src/login.php
+++ src/login.php
@@ -16,59 +16,57 @@ $validated = false;
 
     $error = "";
 
-    if(isset($_POST['nigiri'])) {
-    }
-    if(isset($_POST['california'])) {
-    }    
-    if(isset($_POST['tempura'])) {
-    } 
-    if(isset($_POST['avocado'])) {
-    } 
-
 if(isset($_POST['submit'])){
     $temp_nigiri = htmlspecialchars($_POST['nigiri']);
     $temp_california = htmlspecialchars($_POST['california']);
     $temp_tempura = htmlspecialchars($_POST['tempura']);
-    $temp_avocado = htmlspecialchars($_POST['avocado']);    
+    $temp_avocado = htmlspecialchars($_POST['avocado']);
 
-    if(is_numeric($temp_nigiri) == true){
-            if($temp_nigiri <= 50){
-            $price_nigiri = $temp_nigiri;
-        }else{
-            $error .= "<br>Maximum order is reached";
-            }
-        }else{
-            $error .= "<br>Please add a correct amount";
-            }         
-    
-    if(is_numeric($temp_california) == true){
-            if($temp_california <= 50){
-            $price_cali = $temp_california;
-        }else{
-            $error .= "<br>Maximum order is reached";
-            }
-        }else{
-            $error .= "<br>Please add a correct amount";
+    if(isset($_POST['nigiri'])) {
+        if(is_numeric($temp_nigiri) == true){
+                if($temp_nigiri <= 50){
+                $price_nigiri = $temp_nigiri;
+            }else{
+                $error .= "<br>Maximum order is reached";
+                }
+            }else{
+                $error .= "<br>Please add a correct amount";
+                }     
+            }    
+    if(isset($_POST['california'])) {
+        if(is_numeric($temp_california) == true){
+                if($temp_california <= 50){
+                $price_cali = $temp_california;
+            }else{
+                $error .= "<br>Maximum order is reached";
+                }
+            }else{
+                $error .= "<br>Please add a correct amount";
+                }
             }
             
-    if(is_numeric($temp_tempura) == true){
-            if($temp_tempura <= 50){
-            $price_tempura = $temp_tempura;
-        }else{
-            $error .= "<br>Maximum order is reached>";
+    if(isset($_POST['tempura'])) {        
+        if(is_numeric($temp_tempura) == true){
+                if($temp_tempura <= 50){
+                $price_tempura = $temp_tempura;
+            }else{
+                $error .= "<br>Maximum order is reached>";
+                }
+            }else{
+                $error .= "<br>Please add a correct amount";
+                }
             }
-        }else{
-            $error .= "<br>Please add a correct amount";
-            }
-    
-    if(is_numeric($temp_avocado) == true){
-            if($temp_avocado <= 50){
-            $price_avocado = $temp_avocado;
-        }else{
-            $error .= "<br>Maximum order is reached>";
-            }
-        }else{
-            $error .= "<br>Please add a correct amount";
+         
+    if(isset($_POST['avocado'])) {
+        if(is_numeric($temp_avocado) == true){
+                if($temp_avocado <= 50){
+                $price_avocado = $temp_avocado;
+            }else{
+                $error .= "<br>Maximum order is reached>";
+                }
+            }else{
+                $error .= "<br>Please add a correct amount";
+                }
             }
 
     if(empty($error)){
@@ -77,9 +75,16 @@ if(isset($_POST['submit'])){
         }
 
         if ($validated == true) {
-            $order = $n+$c+$t+$a; 
+            $nigiri_amount = $temp_nigiri * $price_nigiri;
+
+            echo " $nigiri_amount "
+            $order = $price_nigiri + $price_cali + $price_tempura + $price_avocado; 
             echo "Total order = " . $order . "<br>"; 
             }
+
+            $nigiri_amount = $temp_nigiri * $price_nigiri;
+
+            echo " $nigiri_amount "
 
 ?>
 
